605d29dcfd5f7f097e6e85a0c14f3a4ef17d639e
Ben Hall <ben@benhall.me.uk>
2012-10-24 17:16:18 +0100

    Added methods for other options of cookies

--- ga-cookify.js
+++ ga-cookify.js
@@ -1,8 +1,11 @@
 require('./extensions');
 var cookieName = '__utmz';
-var section = 'utmcsr';
+var source = 'utmcsr';
+var campaign = 'utmccn';
+var medium = 'utmcmd';
+var keyword = 'utmctr';
 
-exports.getSourceCookie = function(cookies) {
+var get = function(cookies, name) {
   var result;
   var cookie = cookies[cookieName];
   if(cookie === undefined)
@@ -10,10 +13,36 @@ exports.getSourceCookie = function(cookies) {
 
   var sourceBlock = cookie.split('=');
   sourceBlock.forEach(function(s, i) {
-    if(s.indexOf(section) > -1) {
+    if(s.indexOf(name) > -1) {
       var block = sourceBlock[i + 1].split('|')[0];
       result = block;
     }
   });
+
   return result;
+};
+
+var getAll = exports.getAll = function(cookies) {
+  return {
+    source: getSource(cookies),
+    campaign: getCampaign(cookies),
+    medium: getMedium(cookies),
+    keyword: getKeyword(cookies)
+  };
+};
+
+var getSource = exports.getSource = function(cookies) {
+  return get(cookies, source);
+};
+
+var getCampaign = exports.getCampaign = function(cookies) {
+  return get(cookies, campaign);
+};
+
+var getMedium = exports.getMedium = function(cookies) {
+  return get(cookies, medium);
+};
+
+var getKeyword = exports.getKeyword = function(cookies) {
+  return get(cookies, keyword);
 };
--- tests/ga-Cookify-tests.js
+++ tests/ga-Cookify-tests.js
@@ -6,17 +6,46 @@ describe('GA Cookie Parser', function () {
       __utma: '69927821.813633998.1348646399.1350058484.1350646356.38',
       __utmb: '69927821.9.10.1350646356',
       __utmc: '69927821',
-      __utmz: '69927821.1350029525.34.15.utmcsr=0.0.0.0:3000|utmccn=(referral)|utmcmd=referral|utmcct=/login' };
+      __utmz: '69927821.1350029525.34.15.utmcsr=0.0.0.0:3000|utmccn=(referral)|utmcmd=referral|utmcct=/login|utmctr=keywordtest' };
 
   it('returns an empty string if cookie does not exist', function(done) {
-    var s = gaCookify.getSourceCookie({});
+    var s = gaCookify.getSource({});
     s.should.eql('');
     done();
   });
 
-  it('returns source of request', function (done) {
-    var s = gaCookify.getSourceCookie(example);
+  it('returns source', function (done) {
+    var s = gaCookify.getSource(example);
     s.should.eql('0.0.0.0:3000');
+    done();
+  });
+
+  it('returns campaign', function (done) {
+    var s = gaCookify.getCampaign(example);
+    s.should.eql('(referral)');
+    done();
+  });
+
+  it('returns medium', function (done) {
+    var s = gaCookify.getMedium(example);
+    s.should.eql('referral');
+    done();
+  });
+
+  it('returns keyword', function (done) {
+    var s = gaCookify.getKeyword(example);
+    s.should.eql('keywordtest');
+    done();
+  });
+
+  it('returns all', function (done) {
+    var s = gaCookify.getAll(example);
+    s.should.eql({
+      source: '0.0.0.0:3000',
+      campaign: '(referral)',
+      medium: 'referral',
+      keyword: 'keywordtest'
+    });
     done();
   });
 
